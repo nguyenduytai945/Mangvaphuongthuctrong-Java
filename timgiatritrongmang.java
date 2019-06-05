9db2e2c4c3fed154aad11dc1376531e5503a9d3b
unknown <nguyenduytai945@gmail.com>
2019-06-05 15:00:21 +0700

    add

--- timgiatritrongmang.java
+++ timgiatritrongmang.java
@@ -3,17 +3,17 @@ public class timgiatritrongmang {
     public static void main(String[] args) {
         String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a name’s student:");
+        System.out.print("Nhập tên học sinh:");
         String input_name = scanner.nextLine();
         boolean isExist = false;
         for (int i = 0; i < students.length; i++) {
             if (students[i].equals(input_name)) {
-                System.out.println("Position of the students in the list " + input_name + " is: " + (i + 1));
+                System.out.println("Vị trí của các sinh viên trong danh sách " + input_name + " is: " + (i + 1));
                 isExist = true;
                 break;
             }
         }
         if (!isExist)
-            System.out.println("Not found" + input_name + " in the list.");
+            System.out.println("Không tìm thấy" + input_name + " trong danh sách.");
     }
 }
